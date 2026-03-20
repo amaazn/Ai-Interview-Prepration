e3f97c4d4a2dc2064979428c35e6d5d27495bb3a
Amaan Ahmed <anonymousdarkzone1@gmail.com>
2026-03-20 07:49:49 +0530

    error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interview-prepration-frontend.onrender.com",
     credentials: true
 }))
 
