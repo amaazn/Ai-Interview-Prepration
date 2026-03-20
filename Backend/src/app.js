35fc3c95ad652f9ae7304fe2fc736d936dc3b171
Amaan Ahmed <anonymousdarkzone1@gmail.com>
2026-03-20 08:14:05 +0530

    final commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,27 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+// app.use(cors({
+//     origin: ["https://ai-interview-prepration-frontend.onrender.com", "http://localhost:5173"],
+//     credentials: true
+// }))
+
 app.use(cors({
-    origin: ["https://ai-interview-prepration-frontend.onrender.com", "http://localhost:5173"],
-    credentials: true
-}))
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "https://ai-interview-prepration-frontend.onrender.com",
+      "http://localhost:5173"
+    ];
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
@@ -20,6 +37,8 @@ const interviewRouter = require("./routes/interview.routes")
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 module.exports = app
